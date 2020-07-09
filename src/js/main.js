34ee7a325d14a042b523fe7b05470ec461b52bdd
IvanovVlad <rr.rr0@mail.ru>
2020-07-09 15:23:55 +0400

    Added rocket choose interactions

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+'use strict'
+
 let currentIndex = 0;
 let menus = [];
 const options = document.querySelectorAll('.option');
@@ -59,12 +61,52 @@ start();
 openMenu(1);
 
 /* ------ */
-
 class Rocket {
     teamNumber;
     speed;
     name;
     icon;
 
-    launch() {}
+    launch() { }
 }
+
+const assembleButton = document.querySelector('#assemble-rocket-button');
+
+assembleButton.addEventListener('click', () => {
+    const checkedRadio = document.querySelector('.spaceships-container input[type="radio"]:checked');
+    if (checkedRadio) {
+        showRocket(rockets[parseInt(checkedRadio.id.replace('ss', ''))]);
+    }
+});
+
+function showRocket(rocket) {
+    const tile = document.querySelector('#rocket .info-tile__content');
+    const tmp = tile.querySelectorAll('span.text');
+
+    tile.querySelector('.info-tile__image').src = rocket.icon;
+    tmp[1].innerText = rocket.name;
+    tmp[3].innerText = rocket.speed;
+    tmp[5].innerText = rocket.teamNumber;
+}
+
+const spaceships = document.querySelectorAll('.spaceships-container .info-tile');
+
+function extractParameters(spaceships) {
+    const spaceshipsArray = [];
+
+    spaceships.forEach((ss) => {
+        const tmp = ss.querySelectorAll('span.text');
+
+        const rocket = new Rocket();
+        rocket.name = tmp[1].innerText;
+        rocket.speed = tmp[3].innerText;
+        rocket.teamNumber = tmp[5].innerText;
+        rocket.icon = ss.querySelector('.spaceships-option__image').src;
+
+        spaceshipsArray.push(rocket);
+    });
+
+    return spaceshipsArray;
+}
+
+const rockets = extractParameters(spaceships);
